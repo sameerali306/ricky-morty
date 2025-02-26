7ec9e0d049c446042c3616df2831cf3e7504cdaa
sameerali306 <sameeraly240@gmail.com>
2025-02-26 20:12:54 +0500

    add

--- rick/src/container/homepage/index.jsx
+++ rick/src/container/homepage/index.jsx
@@ -1,12 +1,11 @@
 // import MainLayout from "./container/layout";
 import MainLayout from "../layout";
 import Cards from "../../components/Card/Card";
-import Footer from "../../components/Footer/Footer";
 function HomePage() {
   return (
     <MainLayout>
       <Cards/>
-      <Cards/>
+      <Cards/>~
     </MainLayout>
   );
 }
