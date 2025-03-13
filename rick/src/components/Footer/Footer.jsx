7c2872b0171f69e6a43048f7aaabbdc52e56e322
sameerali306 <sameeraly240@gmail.com>
2025-03-13 23:52:16 +0500

    origin

--- rick/src/components/Footer/Footer.jsx
+++ rick/src/components/Footer/Footer.jsx
@@ -1,36 +1,30 @@
 import Heading from "../heading/Heading";
 import { Row, Col } from "antd";
 import Image from "../Image/Image";
-import { Typography } from "antd";
-const { Link } = Typography;
+import { Link } from "react-router";  // Use Link from react-router-dom to navigate to the profile page
 import img from "../../assets/react.svg";
 import { useSelector } from "react-redux";
+
 const Footer = () => {
   const recentVisitedProfiles = useSelector(
     (state) => state.characters.recentVisitedProfile
-    
   );
- 
-  
+
   return (
     <>
-      <Heading level={4} title=" Recently visited profiles:" />
+      <Heading level={4} title="Recently visited profiles:" />
       <Row gutter={[24, 8]}>
-        {recentVisitedProfiles.slice(0,10).map((profile,index)=>{
-          
-          <Col
-          key={index}
-          style={{
-            margin: "12px 0",
-          }}
-        >
-          <Image width={30} height={30} imageUrl={profile.image || img} />
-          <Link href="https://ant.design" target="_blank">
-            {profile.label}
-          </Link>
-        </Col>
+        {recentVisitedProfiles.slice(0, 10).map((profile, index) => {
+          return (
+            <Col key={index} style={{ margin: "12px 0" }}>
+              <Image width={30} height={30} imageUrl={profile.image || img} />
+              {/* Update the link to navigate to the Profile component of the clicked character */}
+              <Link to={`/profile/${profile.id}`}>
+                {profile.label}
+              </Link>
+            </Col>
+          );
         })}
-        
       </Row>
     </>
   );
